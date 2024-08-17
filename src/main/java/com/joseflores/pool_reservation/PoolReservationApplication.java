2be810aa0c615112c65689ada8b536d8c28dbfb2
Jose_Flores <josemanuelfloreschacon158@gmail.com>
2024-08-17 16:05:25 -0500

    first commit

---
+++ src/main/java/com/joseflores/pool_reservation/PoolReservationApplication.java
@@ -0,0 +1,13 @@
+package com.joseflores.pool_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolReservationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/joseflores/pool_reservation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.joseflores.pool_reservation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PoolReservationApplication.class);
+	}
+
+}
